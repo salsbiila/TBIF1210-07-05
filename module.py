d59a0c5d8bc5e10560554fa558b1b788ab519cc3
Salsabiila <salsabiilabslmh@gmail.com>
2023-04-23 21:24:39 +0700

    revisi -s

--- module.py
+++ module.py
@@ -45,52 +45,53 @@ def makeList(file, delim = ';'):
         currentLength += 1
     return listData
 
-def updateUserDataList(uname, password, jinRole, list, file = userData):
+def updateDataList3(param1, param2, param3, list, file = userData):
     newListData = [['', '', ''] for i in range (rowCount(file)+1)]
     for row in range(rowCount(file)):
         for idx in range(3):    #idx adalah kolom
             newListData[row][idx] = list[row][idx]
-    newListData[rowCount(file)][0] = uname
-    newListData[rowCount(file)][1] = password
-    newListData[rowCount(file)][2] = jinRole
+    newListData[rowCount(file)][0] = param1
+    newListData[rowCount(file)][1] = param2
+    newListData[rowCount(file)][2] = param3
 
     return newListData
 
-def updateUserData(uname, password, jinRole, file = userData):
-    file += f'\n{uname};{password};{jinRole}'
+def updateData3(param1, param2, param3, file = userData):
+    file += f'\n{param1};{param2};{param3}'
 
     return file
 
-def updateCandiDataList(id, pembuat, pasir, batu, air, list, file = candiData):
+def updateDataList5(param1, param2, param3, param4, param5, list, file = candiData):
     newListData = [['', '', ''] for i in range (rowCount(file)+1)]
     for row in range(rowCount(file)):
         for idx in range(5):    #idx adalah kolom
             newListData[row][idx] = list[row][idx]
-    newListData[rowCount(file)][0] = id
-    newListData[rowCount(file)][1] = pembuat
-    newListData[rowCount(file)][2] = pasir
-    newListData[rowCount(file)][3] = batu
-    newListData[rowCount(file)][4] = air
+    newListData[rowCount(file)][0] = param1
+    newListData[rowCount(file)][1] = param2
+    newListData[rowCount(file)][2] = param3
+    newListData[rowCount(file)][3] = param4
+    newListData[rowCount(file)][4] = param5
 
     return newListData
 
-def updateCandiData(id, pembuat, pasir, batu, air, file = candiData):
-    file += f'\n{id};{pembuat};{pasir};{batu};{air}'
+def updateData5(param1, param2, param3, param4, param5, file = candiData):
+    file += f'\n{param1};{param2};{param3};{param4};{param5}'
 
     return file
 
-def updateBahanDataList(nama, deskripsi, jumlah, list, file = bahanData):
-    newListData = [['', '', ''] for i in range (rowCount(file)+1)]
-    for row in range(rowCount(file)):
-        for idx in range(3):    #idx adalah kolom
-            newListData[row][idx] = list[row][idx]
-    newListData[rowCount(file)][0] = nama
-    newListData[rowCount(file)][1] = deskripsi
-    newListData[rowCount(file)][2] = jumlah
-
-    return newListData
-
-def updateBahanData(nama, deskripsi, jumlah, file = bahanData):
-    file += f'\n{nama};{deskripsi};{jumlah}'
-
-    return file
+def deleteUser(uname, list, file = userData):
+    deletionCount = 0
+    for i in range(1, rowCount(file)):
+        if list[i][0] == uname:
+            list[i] = None
+            deletionCount += 1
+    
+    newListData = [['', '', ''] for i in range (rowCount(file)-deletionCount)]
+    
+    row = 0
+    for i in range(rowCount(file)):
+        if list[i] != None:
+            newListData[row] = list[i]
+            row += 1
+    
+    return newListData #belum selesai
