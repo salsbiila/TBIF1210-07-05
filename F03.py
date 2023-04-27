4d110f0bcf85c8665b6b9775cb1b3ac3110bdba3
Salsabiila <salsabiilabslmh@gmail.com>
2023-04-27 14:15:48 +0700

    revisi

--- F03.py
+++ F03.py
@@ -1,27 +1,28 @@
 from module import *
 import time
 
-file = open('user.csv', 'r')
-userData = file.read()
-userDataList = makeList(userData)
-dataSum = rowCount(userData)
-
-def unameExists(uname, data = userDataList):    # fungsi untuk mengecek apakah username sudah ada atau belum
-    for i in range(1, dataSum):
-        if data[i][0] == uname:
+def unameExists(uname, sum, dataList):    # fungsi untuk mengecek apakah username sudah ada atau belum
+    for i in range(1, sum):
+        print(dataList[i][0])
+        if dataList[i][0] == uname:
             return True
     return False
 
-def jinAccount(data):   # fungsi yang menghandle pembuatan akun
+def jinAccount(dataList, sum):   # fungsi yang menghandle pembuatan akun
     uname = input('Masukkan username jin: ')
-    while unameExists(uname, data):
+    while unameExists(uname, sum, dataList):
+        print()
         print(f'Username “{uname}” sudah diambil!')
+        print()
         uname = input('Masukkan username jin: ')
     
     password = input('Masukkan password jin: ')
+    print()
     while len(password) < 5 or len(password) > 25:
         print('Password panjangnya harus 5-25 karakter!')
+        print()
         password = input('Masukkan password jin: ')
+        print()
     
     print('Mengumpulkan sesajen...')
     time.sleep(1)
@@ -30,37 +31,44 @@ def jinAccount(data):   # fungsi yang menghandle pembuatan akun
     print('Membacakan mantra...')
     time.sleep(1)
 
+    print()
     print(f'Jin {uname} berhasil dipanggil!')
 
     return uname, password
 
-def summonJin(data = userDataList):
-    if dataSum == 103:
+def summonJin(sum, dataList, data):
+    if sum == 103:
         print('Jumlah Jin telah maksimal! (100 jin). Bandung tidak dapat men-summon lebih dari itu')
     else:
         print("Jenis jin yang dapat dipanggil:")
         print(' (1) Pengumpul - Bertugas mengumpulkan bahan bangunan')
         print(' (2) Pembangun - Bertugas membangun candi')
+        print()
 
         jinType = int(input('Masukkan nomor jenis jin yang ingin dipanggil: '))
+        print()
 
         while jinType != 1 and jinType != 2:
             print(f'Tidak ada jenis jin bernomor “{jinType}”!')
+            print()
 
             jinType = int(input('Masukkan nomor jenis jin yang ingin dipanggil: '))
+            print()
         
         if jinType == 1:
             print('Memilih jin “Pengumpul”.')
-            uname, password = jinAccount(data)
-            newDataList = updateDataList3(uname, password, 'jin_pengumpul', userDataList)
-            newDataStr = updateData3(uname, password, 'jin_pengumpul')
+            print()
+            uname, password = jinAccount(dataList, sum)
+            newDataList = updateDataList3(uname, password, 'jin_pengumpul', dataList, data)
+            newDataStr = updateData3(uname, password, 'jin_pengumpul', data)
 
             return newDataList, newDataStr
 
         elif jinType == 2:
             print('Memilih jin “Pembangun”.')
-            uname, password = jinAccount(data)
-            newDataList = updateDataList3(uname, password, 'jin_pembangun', userDataList)
-            newDataStr = updateData3(uname, password, 'jin_pembangun')
+            print()
+            uname, password = jinAccount(dataList, sum)
+            newDataList = updateDataList3(uname, password, 'jin_pembangun', dataList, data)
+            newDataStr = updateData3(uname, password, 'jin_pembangun', data)
 
             return newDataList, newDataStr
