0f59c9f5f6524ad2c55dd78d2c623dc9ce0c6dc9
Abdullah_Mubarak <brkmanics@gmail.com>
2023-04-28 11:03:15 +0700

    f10

--- f10.py
+++ f10.py
@@ -1,50 +1,29 @@
-from module import Bacafile, uang, maxmin, pisah, leng, ubah, ppop
+from module import *
 
 global data, jumlahcandi, listharga
 
-data = Bacafile("candi.csv")
-jumlahCandi = -1
-toPas = 0
-toAir = 0
-toBat = 0
-listharga = ""
-c = 0
 
-for line in data:
-    jumlahCandi += 1
-    linedata = pisah(ubah(line, "\n", ""), ",")
-    if c > 0:
-        toPas += int(linedata[2])
-        toAir += int(linedata[4])
-        toBat += int(linedata[3])
-        listharga += str(int(linedata[2]) * 1000 + int(linedata[3]) * 15000 + int(linedata[4]) * 7500) + ","
-    c += 1
+def harga(pasir,batu,air):
+    return pasir*1000 + batu*15000 + air*7500
 
-def jumlahcandi():
-    return jumlahCandi
+def laporancandi(datafile, datasum):
+    toPas, toBat, toAir = 0
+    if datasum == 1:
+        idmax, idmin = "-"
+    else:      
+        max = 0
+        min = harga(int(datafile[1][2]), int(datafile[1][3]), int(datafile[1][4]))
+        Listharga = pisah(ppop(listharga), ",")
 
-def laporancandi():
-    data = Bacafile("candi.csv")
-    c = 0
-    idmax = 0
-    Listharga = pisah(ppop(listharga), ",")
-
-    for line in data:
-        linedata = pisah(ubah(line, "\n", ""), ",")
-        if c > 0:
-            if jumlahCandi > 0:
-                harga = int(linedata[2]) * 1000 + int(linedata[3]) * 15000 + int(linedata[4]) * 7500
-                if harga == maxmin(Listharga)[0]:
-                    maxharga = harga
-                    idmax = linedata[0]
-                elif harga == maxmin(Listharga)[1]:
-                    minharga = harga
-                    idmin = linedata[0]
-            else:
-                idmax = "-"
-                idmin = "-"
-                maxharga = 0
-                minharga = 0
-        c += 1
-    print(f"Total Candi: {jumlahCandi}\nTotal Pasir yang digunakan: {toPas}\nTotal Batu yang digunakan: {toBat}\nTotal Air yang digunakan: {toAir}\nID Candi Termahal: {idmax} (Rp {uang(str(maxharga))})\nID Candi Termurah: {idmin} (Rp {uang(str(minharga))})")
-laporancandi()
+        for i in range(1, datasum - 1):
+            harga = harga(int(datafile[1][2]), int(datafile[1][3]), int(datafile[1][4]))
+            if harga > max:
+                maxharga = harga
+                idmax = datafile[i][0]
+            if harga < min:
+                minharga = harga
+                idmin = datafile[i][0]
+            toPas += datafile[i][2]
+            toBat += datafile[i][3]
+            toAir += datafile[i][4]
+    print(f"Total Candi: {datasum - 1}\nTotal Pasir yang digunakan: {toPas}\nTotal Batu yang digunakan: {toBat}\nTotal Air yang digunakan: {toAir}\nID Candi Termahal: {idmax} (Rp {uang(str(maxharga))})\nID Candi Termurah: {idmin} (Rp {uang(str(minharga))})")
