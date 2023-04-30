104cdea2619edc292cc24edfed0c224c3de416d2
Salsabiila <salsabiilabslmh@gmail.com>
2023-04-30 10:50:04 +0700

    .

--- F04.py
+++ F04.py
@@ -1,19 +1,23 @@
 from module import *
 from F03 import *
 
-def hapusJin(userDataSum, userDataList, candiDataSum, candiDataList):
-    uname = input('Masukkan username jin :')
-    if unameExists(uname, userDataSum, userDataList):
-        userInput = input('Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? ')
-        if userInput == 'Y':
-            newUserSum, newUserDataList = deleteUser(uname, userDataSum, userDataList)
+def hapusJin(userDataSum, userDataList, candiDataSum, candiDataList, role):
+    if role == 'bandung_bondowoso':
+        uname = input('Masukkan username jin :')
+        if unameExists(uname, userDataSum, userDataList):
+            userInput = input('Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? ')
+            if userInput == 'Y':
+                newUserSum, newUserDataList = deleteUser(uname, userDataSum, userDataList)
 
-            newCandiSum, newCandiDataList = deleteCandi(uname, candiDataSum, candiDataList)
+                newCandiSum, newCandiDataList = deleteCandi(uname, candiDataSum, candiDataList)
 
+                print()
+                print('Jin telah berhasil dihapus dari alam gaib.')
+
+                return newUserSum, newUserDataList, newCandiSum, newCandiDataList
+        else:
             print()
-            print('Jin telah berhasil dihapus dari alam gaib.')
-
-            return newUserSum, newUserDataList, newCandiSum, newCandiDataList
+            print('Tidak ada jin dengan username tersebut.')
+    
     else:
-        print()
-        print('Tidak ada jin dengan username tersebut.')
+        print(print('Maaf, anda tidak memiliki kemampuan untuk menghapus jin (ㆀ˘･з･˘)'))
--- F05.py
+++ F05.py
@@ -1,35 +1,39 @@
 from module import *
 from F03 import *
 
-def ubahJin(dataSum, dataList):
-    uname = input('Masukkan username jin : ')
+def ubahJin(dataSum, dataList, role):
+    if role == 'bandung_bondowoso':
+        uname = input('Masukkan username jin : ')
 
-    isProcessed = unameExists(uname, dataSum, dataList)
+        isProcessed = unameExists(uname, dataSum, dataList)
 
-    if isProcessed:
-        for i in range(1, dataSum):
-            if dataList[i][0] == uname:
-                row = i
-        
-        if dataList[row][2] == 'jin_pengumpul':
-            userInput = input(f'Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ')
+        if isProcessed:
+            for i in range(1, dataSum):
+                if dataList[i][0] == uname:
+                    row = i
             
-            if userInput == 'Y':
-                newDataList = switchRole(row, dataSum, dataList)
+            if dataList[row][2] == 'jin_pengumpul':
+                userInput = input(f'Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ')
+                
+                if userInput == 'Y':
+                    newDataList = switchRole(row, dataSum, dataList)
+                
+            elif dataList[row][2] == 'jin_pembangun':
+                userInput = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ')
+
+                if userInput == 'Y':
+                    newDataList = switchRole(row, dataSum, dataList)
+
+            print()
+            print('Jin telah berhasil diubah.')
             
-        elif dataList[row][2] == 'jin_pembangun':
-            userInput = input('Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ')
+            return newDataList
 
-            if userInput == 'Y':
-                newDataList = switchRole(row, dataSum, dataList)
+        else:
+            print()
+            print('Tidak ada jin dengan username tersebut.')
 
-        print()
-        print('Jin telah berhasil diubah.')
-        
-        return newDataList
-
+            return dataList
+    
     else:
-        print()
-        print('Tidak ada jin dengan username tersebut.')
-
-        return dataList
+        print(print('Maaf, anda tidak memiliki kemampuan untuk mengubah jin (ㆀ˘･з･˘)'))
