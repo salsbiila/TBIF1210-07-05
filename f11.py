cdc84c752bdb96bf26a4d2f0231da067ef92d146
Abdullah_Mubarak <brkmanics@gmail.com>
2023-05-02 05:41:39 +0700

    s

--- f11.py
+++ f11.py
@@ -5,7 +5,15 @@ def hancurkancandi(datalist, datasum):
     newdatalist = ["" for i in range(datasum - 1)]
     i = 0
 
+    bool = False
     for i in range(datasum-1):
-        if datalist[i][0] != A:
-            newdatalist = datalist[i][0]
-    return newdatalist
+        if datalist[i][0] == A:
+            bool = True
+    if bool == True:
+        for i in range(datasum-1):
+            if datalist[i][0] != A:
+                newdatalist = datalist[i][0]
+        return newdatalist
+    else:
+        print("Id candi tidak ditemukan")
+        return datalist
