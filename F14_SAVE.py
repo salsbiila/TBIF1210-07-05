94a158edab233a23458ede951ccc0e2b96e4b3de
Salsabiila <salsabiilabslmh@gmail.com>
2023-05-02 06:42:51 +0700

    final

--- F14_SAVE.py
+++ F14_SAVE.py
@@ -15,26 +15,14 @@ def saveCandi (candiNeff, candiList, userList, userNeff, bahanList, lenBahanList
     candiFile = open ("candi.csv", 'w') #digunakan untuk membuka file candi
     candiNeff, candiList = deleteCandi('', candiNeff, candiList)
     candiFile.write(makeStr(candiNeff, candiList, 5))
-    """candiFile.write ("id;pembuat;pasir;batu;air\n") #menulis file 
-    for i in range (candiNeff):
-        if candiList [i] != ("", "", "", "", ""):
-            string = str (candiList[i][0] + ";" + candiList[i][1]+ ";" + str (candiList [1][2]) + ";" + str (candiList[1][3]) + ";" + str (candiList[1][4]))
-            candiFile.write (string + '\n')"""
+    
     bahanBangunanFile = open ("bahan_bangunan.csv", 'w') #digunakan untuk membuka file bahan bangunan
     lenBahanList, bahanList = deleteUser('', lenBahanList, bahanList)
     bahanBangunanFile.write(makeStr(lenBahanList, bahanList, 3))
-    """bahanBangunanFile.write ("nama;deskripsi;jumlah\n") #menulis file
-    for i in range (lenBahanList):
-        if bahanList != ("", "", ""):
-            string = bahanList [i][0] + ';' + bahanList [i][1] + ';' + str (bahanList [i][2])
-            bahanBangunanFile.write(string+"\n")"""
+    
     userFile = open ("user.csv", 'w') #digunakan untuk membuka file user 
     userFile.write(makeStr(userNeff, userList, 3, False))
-    """userFile.write ("username;password;role\n") #menulis file
-    for i in range (userNeff):
-        if userList [i] != ("", "", ""):
-            string = userList [i][0] + ';' + userList [i][1] + ';' + userList[i][2]
-            userFile.write (string + "\n")"""
+    
     userFile.close() #digunakan untuk menutup file user
     candiFile.close() #untuk menutup file candi
     bahanBangunanFile.close() #untuk menutup file bahan bangunan
